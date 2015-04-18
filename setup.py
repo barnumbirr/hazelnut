1c3e7fbf9d7c80add641e3f5c065587bac62ccc1
Eugene Eeo <packwolf58@gmail.com>
2015-04-18 10:40:25 +0800

    remove leading spaces in front of = operators

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@
 from distutils.core import setup
 
 setup(
-	name = 'hazelnut',
-	version = '0.2',
-	url = 'https://github.com/c0ding/hazelnut',
-	download_url = 'https://github.com/c0ding/hazelnut/archive/master.zip',
-	author = 'Martin Simon',
-	author_email = 'me@martinsimon.me',
-	license = 'Apache v2.0 License',
-	packages = ['hazelnut'],
-	description = 'A pythonic library to parse /proc/meminfo',
-	long_description = file('README.md','r').read(),
-	keywords = ['memory', 'RAM', 'system information', 'meminfo', '/proc'],
+    name='hazelnut',
+    version='0.2',
+    url='https://github.com/c0ding/hazelnut',
+    download_url='https://github.com/c0ding/hazelnut/archive/master.zip',
+    author='Martin Simon',
+    author_email='me@martinsimon.me',
+    license='Apache v2.0 License',
+    packages=['hazelnut'],
+    description='A pythonic library to parse /proc/meminfo',
+    long_description=file('README.md','r').read(),
+    keywords=['memory', 'RAM', 'system information', 'meminfo', '/proc'],
 )
